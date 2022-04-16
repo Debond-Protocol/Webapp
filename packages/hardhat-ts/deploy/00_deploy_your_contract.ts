d719c6696788342b668b364f08aa4e4b8be7ddf4
lesquive77 <les.quive77@gmail.com>
2022-04-16 14:39:38 +0200

    .gitignore is now working

--- packages/hardhat-ts/deploy/00_deploy_your_contract.ts
+++ packages/hardhat-ts/deploy/00_deploy_your_contract.ts
@@ -5,7 +5,7 @@ const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { getNamedAccounts, deployments } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-  /*await deploy('YourContract', {
+  /* await deploy('YourContract', {
     // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
     from: deployer,
     // args: ["Hello"],
--- packages/vite-app-ts/src/generated/external-contracts/esm/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Signer } from 'ethers';
-import * as types from './types';
-export declare function getContract(address: string, abi: object, defaultSigner: Signer): any;
-export declare type MainnetSdk = ReturnType<typeof getMainnetSdk>;
-export declare function getMainnetSdk(defaultSigner: Signer): {
-    DAI: types.DAI;
-    UNI: types.UNI;
-};
