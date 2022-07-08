890e6e3a53412427894aa452dc0ac3d3e77f6117
lesquive77 <les.quive77@gmail.com>
2022-07-08 08:58:09 +0200

    mint with amount parameter

--- packages/hardhat-ts/contracts/MysteryBoxToken.sol
+++ packages/hardhat-ts/contracts/MysteryBoxToken.sol
@@ -7,27 +7,32 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 contract MysteryBoxToken is ERC20, Ownable {
-  //    struct SaleConfig {
-  //        uint256 startTime;
-  //        uint256 duration;
-  //    }
-
   using ECDSA for bytes32;
 
   uint256 public startingTime;
   uint256 public endingTime;
   uint256 public duration;
-  bool public saleOn = true;
+  bool public saleOn;
   mapping(address => bool) public discountClaimed;
+  bool hasStarted;
 
   constructor(
     string memory name_,
     string memory symbol_,
     uint256 _duration
   ) ERC20(name_, symbol_) {
-    startingTime = block.timestamp;
+    //        startingTime = block.timestamp;
     duration = _duration;
-    endingTime = block.timestamp + _duration;
+  }
+
+  /**
+   * @notice this function is only called once
+   */
+  function startTime() external onlyOwner {
+    require(!hasStarted);
+    startingTime = block.timestamp;
+    endingTime = block.timestamp + duration;
+    hasStarted = true;
   }
 
   modifier isDiscountAuthorized(uint256 discount, bytes memory signature) {
@@ -35,7 +40,7 @@ contract MysteryBoxToken is ERC20, Ownable {
     _;
   }
 
-  function verifySignature(uint256 discount, bytes memory signature) public view returns (address) {
+  function verifySignature(uint256 discount, bytes memory signature) internal view returns (address) {
     return keccak256(abi.encodePacked(address(this), msg.sender, discount)).toEthSignedMessageHash().recover(signature);
   }
 
@@ -44,23 +49,28 @@ contract MysteryBoxToken is ERC20, Ownable {
   }
 
   //@YU: This function gives the price now
-  function getMintingPrice() internal view virtual returns (uint256) {
+  function getMintingPrice() public view virtual returns (uint256) {
+    if (startingTime == 0) {
+      return 2e17;
+    }
     //The percentage of time passed
     uint256 percentageTimePassed = ((block.timestamp - startingTime) * 100) / duration;
     return 2e17 + (3e17 * percentageTimePassed) / 100;
   }
 
   //front end get minting price () first then put the price in to payable parameter
-  function mint() external payable {
-    require(block.timestamp < endingTime, "Sale has expired");
-    require(msg.value >= getMintingPrice());
+  function mint(uint256 amount) external payable {
+    require(saleOn, "Sale is off");
+    require(msg.value >= amount * getMintingPrice());
     //after the transfer of eth mint token.
-    _mint(msg.sender, 1);
+    _mint(msg.sender, amount); //todo done here
   }
 
   function mintDiscount(uint256 _discount, bytes memory _signature) external payable isDiscountAuthorized(_discount, _signature) {
     require(saleOn == true, "Sale is Off");
-    require(block.timestamp < endingTime, "Sale has expired");
+    if (endingTime != 0) {
+      require(block.timestamp < endingTime, "Sale has expired");
+    }
     require(!discountClaimed[msg.sender], "caller already used his discount");
     require(_discount > 0 && _discount < 100, "discount must be between 0 and 100");
     require(msg.value >= (getMintingPrice() * (100 - _discount)) / 100, "missing ETH");
@@ -83,25 +93,4 @@ contract MysteryBoxToken is ERC20, Ownable {
     require(_amount > 0, "No ETH to Withdraw");
     payable(_msgSender()).transfer(_amount);
   }
-
-  //    function getPrice() public view returns (uint256) {
-  //        uint256 _price;
-  //        SaleConfig memory _saleConfig = saleConfig;
-  //        if (block.timestamp <= _saleConfig.startTime + 6 hours) {
-  //            _price = 0.5 ether;
-  //        } else if (
-  //            (block.timestamp >= _saleConfig.startTime + 6 hours) &&
-  //            (block.timestamp <= _saleConfig.startTime + 12 hours)
-  //        ) {
-  //            _price = 0.4 ether;
-  //        } else if (
-  //            (block.timestamp > _saleConfig.startTime + 12 hours) &&
-  //            (block.timestamp <= _saleConfig.startTime + 18 hours)
-  //        ) {
-  //            _price = 0.3 ether;
-  //        } else {
-  //            _price = 0.3 ether;
-  //        }
-  //        return _price;
-  //    }
 }
