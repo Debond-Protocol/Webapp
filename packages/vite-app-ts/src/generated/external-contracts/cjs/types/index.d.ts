38deb14987643a163f9ab70168290b54a815c7d6
lesquive77 <les.quive77@gmail.com>
2022-08-15 09:15:24 +0200

    ignore generated

--- packages/vite-app-ts/src/generated/external-contracts/cjs/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export type { DAI } from './DAI';
-export type { UNI } from './UNI';
-export { DAI__factory } from './factories/DAI__factory';
-export { UNI__factory } from './factories/UNI__factory';
